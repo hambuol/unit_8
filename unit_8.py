9ff14e01d14df9fe102f58750b3c88eb06ae3d6a
hambuol <oliver.hamburger@ssfs.org>
2016-10-18 10:16:38 -0400

    "added stuff"

--- unit_8.py
+++ unit_8.py
@@ -1,12 +1,53 @@
 import tkinter
+def encodestr():
+    print("sup")
+
+
+def decodestr():
+    print("dude")
+
 
 root = tkinter.Tk()
 
 encode_decodelabel = tkinter.Label(root, text = "Encode/Decode")
 encode_decodelabel.grid(column = 1, row = 1)
 
-encode = tkinter.Label(root, text = "encode:")
+encode = tkinter.Label(root, text = "string to encode:")
 encode.grid(column = 1, row = 2)
+
+
+string = tkinter.StringVar()
+entryfield = tkinter.Entry(root, textvariable = string)
+entryfield.grid(column = 2, row = 2)
+
+key = tkinter.Label(root, text = "key to encode (0-25):")
+key.grid(column = 1, row = 3)
+
+key = tkinter.StringVar()
+entryfield = tkinter.Entry(root, textvariable = key)
+entryfield.grid(column = 2, row = 3)
+
+encodebutton = tkinter.Button(root, text = "encode", command = encodestr)
+encodebutton.grid(column = 2, row = 4)
+
+decode = tkinter.Label(root, text = "string to encode:")
+decode.grid(column = 3, row = 2)
+
+string = tkinter.StringVar()
+entryfield = tkinter.Entry(root, textvariable = string)
+entryfield.grid(column = 4, row = 2)
+
+key = tkinter.Label(root, text = "key to encode (0-25):")
+key.grid(column = 3, row = 3)
+
+
+key = tkinter.StringVar()
+entryfield = tkinter.Entry(root, textvariable = key)
+entryfield.grid(column = 4, row = 3)
+
+decodebutton = tkinter.Button(root, text = "decode", command = decodestr)
+decodebutton.grid(column = 4, row = 4)
+
 
 
 
