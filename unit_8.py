50dc364ae9d29a84e06984138e628a6c3c72ea20
hambuol <oliver.hamburger@ssfs.org>
2016-10-20 10:41:36 -0400

    put more stuff, need to get button working

--- unit_8.py
+++ unit_8.py
@@ -1,6 +1,20 @@
 import tkinter
+n = "abcdefghijklmnopqrstuvwxyz"
 def encodestr():
-    print("sup")
+    string = entry.get()
+    key = entrykey.get()
+    r = n[key:26] + n[0:key]
+    encoded = ""
+    for x in string:
+        encoded += r[n.index(x)]
+
+    r = n[key:26] + n[0:key]
+    encoded = ""
+    for x in string:
+        encoded += r[n.index(x)]
+
+    
+
 
 
 def decodestr():
@@ -16,15 +30,15 @@ encode = tkinter.Label(root, text = "string to encode:")
 encode.grid(column = 1, row = 2)
 
 
-string = tkinter.StringVar()
-entryfield = tkinter.Entry(root, textvariable = string)
+entry = tkinter.StringVar()
+entryfield = tkinter.Entry(root, textvariable = entry)
 entryfield.grid(column = 2, row = 2)
 
 key = tkinter.Label(root, text = "key to encode (0-25):")
 key.grid(column = 1, row = 3)
 
-key = tkinter.StringVar()
-entryfield = tkinter.Entry(root, textvariable = key)
+entrykey = tkinter.IntVar()
+entryfield = tkinter.Entry(root, textvariable = entrykey)
 entryfield.grid(column = 2, row = 3)
 
 encodebutton = tkinter.Button(root, text = "encode", command = encodestr)
@@ -33,8 +47,8 @@ encodebutton.grid(column = 2, row = 4)
 decode = tkinter.Label(root, text = "string to encode:")
 decode.grid(column = 3, row = 2)
 
-string = tkinter.StringVar()
-entryfield = tkinter.Entry(root, textvariable = string)
+entry = tkinter.StringVar()
+entryfield = tkinter.Entry(root, textvariable = entry)
 entryfield.grid(column = 4, row = 2)
 
 key = tkinter.Label(root, text = "key to encode (0-25):")
@@ -48,7 +62,11 @@ entryfield.grid(column = 4, row = 3)
 decodebutton = tkinter.Button(root, text = "decode", command = decodestr)
 decodebutton.grid(column = 4, row = 4)
 
+finallable = tkinter.Label(root, text = "encoded/decoded string:")
+finallable.grid(column = 1, row = 5)
 
+finallabelvalue = tkinter.Label(root, textvariable = encode)
+finallabelvalue.grid(column = 2, row = 5)
 
 
 root.mainloop()
