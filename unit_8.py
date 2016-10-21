f5aa7d4fc4f031f917e240d87f4a5f20e57bf5f6
hambuol <oliver.hamburger@ssfs.org>
2016-10-21 10:47:14 -0400

    got fist button working

--- unit_8.py
+++ unit_8.py
@@ -8,17 +8,23 @@ def encodestr():
     for x in string:
         encoded += r[n.index(x)]
 
-    r = n[key:26] + n[0:key]
-    encoded = ""
-    for x in string:
-        encoded += r[n.index(x)]
+    answer.set(encoded)
+
 
     
 
 
 
 def decodestr():
-    print("dude")
+    string = entry1.get()
+    key = key1.get()
+    r = n[key:26] + n[0:key]
+    decoded = ""
+    for x in string:
+        decoded += n[r.index(x)]
+
+    answer.set(decoded)
+
 
 
 root = tkinter.Tk()
@@ -44,19 +50,19 @@ entryfield.grid(column = 2, row = 3)
 encodebutton = tkinter.Button(root, text = "encode", command = encodestr)
 encodebutton.grid(column = 2, row = 4)
 
-decode = tkinter.Label(root, text = "string to encode:")
+decode = tkinter.Label(root, text = "string to decode:")
 decode.grid(column = 3, row = 2)
 
-entry = tkinter.StringVar()
-entryfield = tkinter.Entry(root, textvariable = entry)
+entry1 = tkinter.StringVar()
+entryfield = tkinter.Entry(root, textvariable = entry1)
 entryfield.grid(column = 4, row = 2)
 
-key = tkinter.Label(root, text = "key to encode (0-25):")
+key = tkinter.Label(root, text = "key to decode (0-25):")
 key.grid(column = 3, row = 3)
 
 
-key = tkinter.StringVar()
-entryfield = tkinter.Entry(root, textvariable = key)
+key1 = tkinter.StringVar()
+entryfield = tkinter.Entry(root, textvariable = key1)
 entryfield.grid(column = 4, row = 3)
 
 decodebutton = tkinter.Button(root, text = "decode", command = decodestr)
@@ -65,7 +71,8 @@ decodebutton.grid(column = 4, row = 4)
 finallable = tkinter.Label(root, text = "encoded/decoded string:")
 finallable.grid(column = 1, row = 5)
 
-finallabelvalue = tkinter.Label(root, textvariable = encode)
+answer = tkinter.StringVar()
+finallabelvalue = tkinter.Label(root, textvariable = answer)
 finallabelvalue.grid(column = 2, row = 5)
 
 
