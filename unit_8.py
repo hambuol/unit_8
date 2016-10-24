723924d80ebbe06bed94573bda153d3cef1bbbc9
hambuol <oliver.hamburger@ssfs.org>
2016-10-24 14:54:55 -0400

    made it look perty

--- unit_8.py
+++ unit_8.py
@@ -41,8 +41,8 @@ root = tkinter.Tk()
 root.title("Encode/Decode Strings")
 
 # adds a label saying "string to encode" and grids it
-encode = tkinter.Label(root, text="string to encode:")
-encode.grid(column=1, row=2)
+encode = tkinter.Label(root, text="String to encode:")
+encode.grid(column=1, row=2, sticky="E")
 
 # creates a entry box for the user to put their string to encode
 entry = tkinter.StringVar()
@@ -50,8 +50,8 @@ entryfield = tkinter.Entry(root, textvariable=entry)
 entryfield.grid(column=2, row=2)
 
 # adds a label saying "key to encode (0-25)
-key = tkinter.Label(root, text="key to encode (0-25):")
-key.grid(column=1, row=3)
+key = tkinter.Label(root, text="Key to encode (0-25):")
+key.grid(column=1, row=3, sticky="E")
 
 # creates a entry box for the user to put their key to encode and grids it
 entrykey = tkinter.IntVar()
@@ -59,12 +59,12 @@ entryfield = tkinter.Entry(root, textvariable=entrykey)
 entryfield.grid(column=2, row=3)
 
 # creates button that encodes string via encodestr function
-encodebutton = tkinter.Button(root, text="encode", command=encodestr)
+encodebutton = tkinter.Button(root, text="Encode", command=encodestr)
 encodebutton.grid(column=2, row=4)
 
 # adds label saying "string to decode" and grids it
-decode = tkinter.Label(root, text="string to decode:")
-decode.grid(column=3, row=2)
+decode = tkinter.Label(root, text="String to decode:")
+decode.grid(column=3, row=2, sticky="E")
 
 # creates a entry box for the user to put their key to decode and grids it
 entry1 = tkinter.StringVar()
@@ -72,8 +72,8 @@ entryfield = tkinter.Entry(root, textvariable=entry1)
 entryfield.grid(column=4, row=2)
 
 # adds label saying "key to decode" and grids it
-key = tkinter.Label(root, text="key to decode (0-25):")
-key.grid(column=3, row=3)
+key = tkinter.Label(root, text="Key to decode (0-25):")
+key.grid(column=3, row=3, sticky="E")
 
 # creates a entry box for the user to pu their key to decode and grids it
 key1 = tkinter.IntVar()
@@ -81,17 +81,17 @@ entryfield = tkinter.Entry(root, textvariable=key1)
 entryfield.grid(column=4, row=3)
 
 # creates a button that decodes the given string via decodestr
-decodebutton = tkinter.Button(root, text="decode", command=decodestr)
+decodebutton = tkinter.Button(root, text="Decode", command=decodestr)
 decodebutton.grid(column=4, row=4)
 
 # adds label saying "encoded/decoded string"
-finallable = tkinter.Label(root, text="encoded/decoded string:")
+finallable = tkinter.Label(root, text="Encoded/Decoded string:")
 finallable.grid(column=1, row=5)
 
-# creates label for encoded/decoded string to be displayed on
+# creates label for encoded/decoded string to be displayed on and puts answer in a larger font
 answer = tkinter.StringVar()
-finallabelvalue = tkinter.Label(root, textvariable=answer)
-finallabelvalue.grid(column=2, row=5)
+finallabelvalue = tkinter.Label(root, textvariable=answer, font="Arial 20 bold")
+finallabelvalue.grid(column=2, row=5, sticky="W")
 
 # runs the main loop for tkinter
 root.mainloop()
